70275c8634c17ff6f8cf65726dfc2bdef4b71003
Daco Harkes <dacoharkes@google.com>
2026-05-06 11:02:21 +0200

    organize link hook

--- pkgs/idle_pirate/hook/link.dart
+++ pkgs/idle_pirate/hook/link.dart
@@ -9,87 +9,149 @@ import 'package:record_use/record_use.dart';
 
 import '../../../tree_shaking_config.dart';
 
-// Top-level constants for lookup
-const _assetsLib = Library('package:idle_pirate/assets/images.dart');
-const _modelsLib = Library('package:idle_pirate/models/item.dart');
-const _soundsLib = Library('package:idle_pirate/assets/sounds.dart');
-
-const _staticIconDef = Class('StaticIcon', _assetsLib);
-const _dynamicIconDef = Class('DynamicIcon', _assetsLib);
-const _itemDef = Class('Item', _modelsLib);
-const _soundDef = Class('Sound', _soundsLib);
-
-const _itemCategory = 'item';
-
 void main(List<String> args) async {
   await link(args, (LinkInput input, LinkOutputBuilder output) async {
     if (!input.config.buildDataAssets) return;
 
     final usages = input.recordedUses;
+    final dataAssets = input.assets.data;
 
     if (usages == null) {
       print(
         'No recorded uses found. Bailing on treeshaking and including all assets.',
       );
-      output.assets.data.addAll(input.assets.data);
-      output.dependencies.addAll(input.assets.data.map((a) => a.file));
+      output.assets.data.addAll(dataAssets);
+      output.dependencies.addAll(dataAssets.map((a) => a.file));
       return;
     }
 
-    final usedStaticImages = _usedStaticImages(usages);
+    treeshakeSounds(dataAssets, usages, output);
+
     final usedItems = _usedItems(usages);
-    final usedSounds = _usedSounds(usages);
 
-    final idsPerCategory = {_itemCategory: usedItems};
+    await treeshakeImages(dataAssets, usages, usedItems, input, output);
 
-    final usedDynamicImages = _usedDynamicImages(usages, idsPerCategory);
+    await treeshakeTranslations(usedItems, output, input);
 
-    final usedImages = {...usedStaticImages, ...usedDynamicImages};
-
-    final imageAssets = _imageAssets(input.assets.data);
-    final soundAssets = _soundAssets(input.assets.data);
-
-    final (handledImages, imageDeps) = await _treeShakeImages(
-      imageAssets,
-      usedImages,
-      input.outputDirectoryShared,
-    );
-    final (handledSounds, soundDeps) = _treeShakeSounds(
-      soundAssets,
-      usedSounds,
-    );
-
-    output.assets.data.addAll(handledImages);
-    output.assets.data.addAll(handledSounds);
-
-    output.dependencies.addAll(imageDeps);
-    output.dependencies.addAll(soundDeps);
-
-    // Produce and route category IDs to pirate_speak!
-    final categoryIds = PirateSpeakCategoryIds({
-      'item': usedItems.toList(),
-    });
-    await categoryIds.sendToLinkHook(output, input.outputDirectoryShared);
-
-    _handleOtherAssets(input.assets.data, output);
+    _handleOtherAssets(dataAssets, output);
   });
 }
 
-void _checkCategories(Iterable<String> categories, String sourceName) {
-  final unknown = categories.where((k) => k != _itemCategory).toList();
-  if (unknown.isNotEmpty) {
-    throw StateError(
-      'Unknown categories in $sourceName: $unknown. You need to handle these in the link hook.',
-    );
-  }
+void treeshakeSounds(
+  Iterable<DataAsset> dataAssets,
+  Recordings usages,
+  LinkOutputBuilder output,
+) {
+  final soundAssets = _soundAssets(dataAssets);
+  final usedSounds = _usedSounds(usages);
+  final (handledSounds, soundDeps) = _filterSounds(
+    soundAssets,
+    usedSounds,
+  );
+  output.assets.data.addAll(handledSounds);
+  output.dependencies.addAll(soundDeps);
 }
 
+Iterable<DataAsset> _soundAssets(Iterable<DataAsset> assets) =>
+    assets.where((a) => a.name.startsWith('assets/sounds/'));
+
+Set<String> _usedSounds(Recordings usages) {
+  const soundDef = Class(
+    'Sound',
+    Library('package:idle_pirate/assets/sounds.dart'),
+  );
+
+  final usedSoundIds = <String>{};
+  final soundInstances = usages.instances[soundDef];
+  if (soundInstances == null) {
+    throw StateError(
+      'No recordings found for $soundDef. You need to handle this in the link hook.',
+    );
+  }
+  for (final instance in soundInstances) {
+    switch (instance) {
+      case InstanceConstantReference(
+        instanceConstant: InstanceConstant(
+          fields: {'id': StringConstant(value: final id)},
+        ),
+      ):
+        usedSoundIds.add(id);
+      case InstanceCreationReference(
+        positionalArguments: [StringConstant(value: final id)],
+      ):
+        usedSoundIds.add(id);
+      case InstanceCreationReference():
+        // Ignore non-const calls
+        break;
+      case _:
+        throw StateError('Cannot safely parse Sound instance: $instance');
+    }
+  }
+  return usedSoundIds;
+}
+
+(List<DataAsset>, Set<Uri>) _filterSounds(
+  Iterable<DataAsset> assets,
+  Set<String> usedSounds,
+) {
+  final outputAssets = <DataAsset>[];
+  final dependencies = <Uri>{};
+
+  if (!enableAudioTreeShaking) {
+    outputAssets.addAll(assets);
+    dependencies.addAll(assets.map((e) => e.file));
+    return (outputAssets, dependencies);
+  }
+
+  for (final asset in assets) {
+    final filename = asset.name.split('/').last;
+    final id = filename.split('.').first;
+
+    dependencies.add(asset.file);
+    if (usedSounds.contains(id)) {
+      print('Keeping sound: ${asset.name}');
+      outputAssets.add(asset);
+    } else {
+      print('Filtering out sound: ${asset.name}');
+    }
+  }
+  return (outputAssets, dependencies);
+}
+
+Future<void> treeshakeImages(
+  Iterable<DataAsset> dataAssets,
+  Recordings usages,
+  Set<String> usedItems,
+  LinkInput input,
+  LinkOutputBuilder output,
+) async {
+  final imageAssets = _imageAssets(dataAssets);
+  final usedStaticImages = _usedStaticImages(usages);
+  final idsPerCategory = {_itemCategory: usedItems};
+  final usedDynamicImages = _usedDynamicImages(usages, idsPerCategory);
+  final usedImages = {...usedStaticImages, ...usedDynamicImages};
+  final (handledImages, imageDeps) = await _filterImages(
+    imageAssets,
+    usedImages,
+    input.outputDirectoryShared,
+  );
+  output.assets.data.addAll(handledImages);
+  output.dependencies.addAll(imageDeps);
+}
+
+Iterable<DataAsset> _imageAssets(Iterable<DataAsset> assets) =>
+    assets.where((a) => a.name.startsWith('assets/images/'));
+
+const _assetsLib = Library('package:idle_pirate/assets/images.dart');
+
 Map<String, double> _usedStaticImages(Recordings usages) {
+  const staticIconDef = Class('StaticIcon', _assetsLib);
+
   final iconSizes = <String, double>{};
-  final staticInstances = usages.instances[_staticIconDef];
+  final staticInstances = usages.instances[staticIconDef];
   if (staticInstances == null) {
     throw StateError(
-      'No recordings found for $_staticIconDef. You need to handle this in the link hook.',
+      'No recordings found for $staticIconDef. You need to handle this in the link hook.',
     );
   }
   for (final instance in staticInstances) {
@@ -121,12 +183,13 @@ Map<String, double> _usedDynamicImages(
   Recordings usages,
   Map<String, Set<String>> idsPerCategory,
 ) {
+  const dynamicIconDef = Class('DynamicIcon', _assetsLib);
   final usedImages = <String, double>{};
   final usedCategories = <String>{};
-  final dynamicInstances = usages.instances[_dynamicIconDef];
+  final dynamicInstances = usages.instances[dynamicIconDef];
   if (dynamicInstances == null) {
     throw StateError(
-      'No recordings found for $_dynamicIconDef. You need to handle this in the link hook.',
+      'No recordings found for $dynamicIconDef. You need to handle this in the link hook.',
     );
   }
   for (final instance in dynamicInstances) {
@@ -171,89 +234,12 @@ Map<String, double> _usedDynamicImages(
     }
   }
 
-  _checkCategories(usedCategories, '$_dynamicIconDef');
+  _checkCategories(usedCategories, '$dynamicIconDef');
 
   return usedImages;
 }
 
-Set<String> _usedItems(Recordings usages) {
-  final usedItemIds = <String>{};
-  final itemInstances = usages.instances[_itemDef];
-  if (itemInstances == null) {
-    throw StateError(
-      'No recordings found for $_itemDef. You need to handle this in the link hook.',
-    );
-  }
-  for (final instance in itemInstances) {
-    switch (instance) {
-      case InstanceConstantReference(
-        instanceConstant: InstanceConstant(
-          fields: {'id': StringConstant(value: final id)},
-        ),
-      ):
-        usedItemIds.add(id);
-      case InstanceCreationReference(
-        namedArguments: {'id': StringConstant(value: final id)},
-      ):
-        usedItemIds.add(id);
-      case _:
-        throw StateError('Cannot safely parse Item instance: $instance');
-    }
-  }
-  return usedItemIds;
-}
-
-Set<String> _usedSounds(Recordings usages) {
-  final usedSoundIds = <String>{};
-  final soundInstances = usages.instances[_soundDef];
-  if (soundInstances == null) {
-    throw StateError(
-      'No recordings found for $_soundDef. You need to handle this in the link hook.',
-    );
-  }
-  for (final instance in soundInstances) {
-    switch (instance) {
-      case InstanceConstantReference(
-        instanceConstant: InstanceConstant(
-          fields: {'id': StringConstant(value: final id)},
-        ),
-      ):
-        usedSoundIds.add(id);
-      case InstanceCreationReference(
-        positionalArguments: [StringConstant(value: final id)],
-      ):
-        usedSoundIds.add(id);
-      case InstanceCreationReference():
-        // Ignore non-const calls
-        break;
-      case _:
-        throw StateError('Cannot safely parse Sound instance: $instance');
-    }
-  }
-  return usedSoundIds;
-}
-
-Iterable<DataAsset> _imageAssets(Iterable<DataAsset> assets) =>
-    assets.where((a) => a.name.startsWith('assets/images/'));
-
-Iterable<DataAsset> _soundAssets(Iterable<DataAsset> assets) =>
-    assets.where((a) => a.name.startsWith('assets/sounds/'));
-
-void _handleOtherAssets(Iterable<DataAsset> assets, LinkOutputBuilder output) {
-  final otherAssets = assets.where(
-    (a) =>
-        !a.name.startsWith('assets/images/') &&
-        !a.name.startsWith('assets/translations/') &&
-        !a.name.startsWith('assets/sounds/'),
-  );
-
-  for (final asset in otherAssets) {
-    print('Unknown asset type: ${asset.name}');
-    output.assets.data.add(asset);
-  }
-}
-
-Future<(List<DataAsset>, Set<Uri>)> _treeShakeImages(
+Future<(List<DataAsset>, Set<Uri>)> _filterImages(
   Iterable<DataAsset> assets,
   Map<String, double> usedImages,
   Uri baseOutputDir,
@@ -324,34 +310,6 @@ Future<(List<DataAsset>, Set<Uri>)> _treeShakeImages(
   return (outputAssets, dependencies);
 }
 
-(List<DataAsset>, Set<Uri>) _treeShakeSounds(
-  Iterable<DataAsset> assets,
-  Set<String> usedSounds,
-) {
-  final outputAssets = <DataAsset>[];
-  final dependencies = <Uri>{};
-
-  if (!enableAudioTreeShaking) {
-    outputAssets.addAll(assets);
-    dependencies.addAll(assets.map((e) => e.file));
-    return (outputAssets, dependencies);
-  }
-
-  for (final asset in assets) {
-    final filename = asset.name.split('/').last;
-    final id = filename.split('.').first;
-
-    dependencies.add(asset.file);
-    if (usedSounds.contains(id)) {
-      print('Keeping sound: ${asset.name}');
-      outputAssets.add(asset);
-    } else {
-      print('Filtering out sound: ${asset.name}');
-    }
-  }
-  return (outputAssets, dependencies);
-}
-
 Future<bool> _resizeIcon(File source, File target, String sizeStr) async {
   print('Resizing asset: ${source.path} to $sizeStr');
   final result = await Process.run('magick', [
@@ -377,4 +335,72 @@ Future<bool> _shouldResize(File source, File target) async {
 
   return sourceTime.isAfter(targetTime) ||
       sourceTime.isAtSameMomentAs(targetTime);
+}
+
+Future<void> treeshakeTranslations(
+  Set<String> usedItems,
+  LinkOutputBuilder output,
+  LinkInput input,
+) async {
+  // Produce and route category IDs to pirate_speak!
+  final categoryIds = PirateSpeakCategoryIds({
+    _itemCategory: [...usedItems],
+  });
+  await categoryIds.sendToLinkHook(output, input.outputDirectoryShared);
+}
+
+const _itemCategory = 'item';
+
+void _checkCategories(Iterable<String> categories, String sourceName) {
+  final unknown = categories.where((k) => k != _itemCategory).toList();
+  if (unknown.isNotEmpty) {
+    throw StateError(
+      'Unknown categories in $sourceName: $unknown. You need to handle these in the link hook.',
+    );
+  }
+}
+
+Set<String> _usedItems(Recordings usages) {
+  const itemDef = Class(
+    'Item',
+    Library('package:idle_pirate/models/item.dart'),
+  );
+
+  final usedItemIds = <String>{};
+  final itemInstances = usages.instances[itemDef];
+  if (itemInstances == null) {
+    throw StateError(
+      'No recordings found for $itemDef. You need to handle this in the link hook.',
+    );
+  }
+  for (final instance in itemInstances) {
+    switch (instance) {
+      case InstanceConstantReference(
+        instanceConstant: InstanceConstant(
+          fields: {'id': StringConstant(value: final id)},
+        ),
+      ):
+        usedItemIds.add(id);
+      case InstanceCreationReference(
+        namedArguments: {'id': StringConstant(value: final id)},
+      ):
+        usedItemIds.add(id);
+      case _:
+        throw StateError('Cannot safely parse Item instance: $instance');
+    }
+  }
+  return usedItemIds;
+}
+
+void _handleOtherAssets(Iterable<DataAsset> assets, LinkOutputBuilder output) {
+  final otherAssets = assets.where(
+    (a) =>
+        !a.name.startsWith('assets/images/') &&
+        !a.name.startsWith('assets/sounds/'),
+  );
+
+  for (final asset in otherAssets) {
+    print('Unknown asset type: ${asset.name}');
+    output.assets.data.add(asset);
+  }
 }
